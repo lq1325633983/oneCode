581868d2b603b6fd0ef792cf2cdb2d6b245936cb
李强 <1325633983@qq.com>
2020-04-04 16:43:00 +0800

    第一次提交了所有文件

---
+++ login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+<script>
+var btn = {
+  template: `
+    <button>按钮啊</button>
+    <p>传值</p>
+  `
+}
+export default {
+  components: {
+    btn: 'btn'
+  },
+  data () {
+    return {}
+  },
+  methods: {}
+}
+</script>
+<style>
+
+</style>
+
